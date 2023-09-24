6f5854a0208fc550706500eff07498d9c1df9c49
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2023-09-24 06:19:27 -0400

    added csv

--- backend/app.py
+++ backend/app.py
@@ -10,9 +10,13 @@ def get_eth_data():
     try:
         endpoint = "https://api.1inch.dev/portfolio/v2/prices/token_prices/time_range?chain_id=1&contract_address=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&currency=usd&granularity=day"
         eth_response = requests.post(endpoint, headers={'Authorization': f'Bearer {config.API_KEY}'})
+        eth_response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code.
         return eth_response.json().get('prices', [])
+    except requests.HTTPError:
+        return jsonify(error="API request failed", details=eth_response.text), 500
     except Exception as error:
-        return jsonify(error=error), 500
+        return jsonify(error=str(error)), 500
+
 
 @app.route('/polyData')
 def get_poly_data():
